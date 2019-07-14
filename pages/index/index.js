02e982cbe5774d6fb3f98b1af0ad28b1b552596e
caijixjc <756035028@qq.com>
2019-07-14 10:56:07 +0800

    首页初步完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,137 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
 Page({
+  
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    slideshow: [],//轮播图
+    menu:[],//菜单栏图片
+    floordata:[],//楼层数据
+    showTop:false
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // 轮播图数据
+  this.getslideData();
+    // 导航栏
+  this.getnavData();
+    // 楼层数据
+    this.getfoorData();
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
+  // 轮播图数据
+  getslideData(){
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+
+      success: res => {
+        const {message} = res.data;
         this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+          slideshow:message
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
+    })
+  },
+  // 导航栏数据
+  getnavData(){
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/catitems',
+     
+      success: res => {
+        const {message} = res.data;
+        this.setData({
+          menu:message
+        })
+      }
+    })
+  },
+  // 楼层数据
+  getfoorData(){
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/floordata',
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: res => {
+        const {message} = res.data;
+        this.setData({
+          floordata:message
+        })
+      }
+    })
+  },
+  // 回到顶部
+  goTop(event){
+    console.log(event);
+    const {top} = event.currentTarget.dataset;
+    wx.pageScrollTo({
+      scrollTop:top,
+      duration:300
+    })
+  },
+  onPageScroll(event){
+    const { scrollTop } = event;
+    if (scrollTop > 200){
+      this.setData({
+        showTop: true
+      })
+    }else{
+      this.setData({
+        showTop: false
       })
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+  
+  
+  
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
   }
 })
